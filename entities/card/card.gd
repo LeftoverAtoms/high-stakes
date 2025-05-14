092765708e4eca479640dba35b424294b9c65942
Adam Calvelage <adamjasoncalvelage@gmail.com>
2025-05-14 14:54:50 -0600

    Cards are now stored as resources.

---
+++ entities/card/card.gd
@@ -0,0 +1,48 @@
+class_name CasinoCard
+extends Resource
+
+enum Suits {
+	# Default.
+	NONE = 0,
+
+	# Standard.
+	CLUBS = 1,
+	DIAMONDS = 2,
+	HEARTS = 3,
+	SPADES = 4,
+}
+
+enum Ranks {
+	# Default.
+	NONE = 0,
+
+	# Standard.
+	ACE = 1,
+	TWO = 2,
+	THREE = 3,
+	FOUR = 4,
+	FIVE = 5,
+	SIX = 6,
+	SEVEN = 7,
+	EIGHT = 8,
+	NINE = 9,
+	TEN = 10,
+	KING = 11,
+	QUEEN = 12,
+	JACK = 13,
+
+	# Wildcard.
+	JOKER = 14,
+}
+
+@export var suit: Suits
+@export var rank: Ranks
+
+@export var front_texture: Texture2D
+@export var back_texture: Texture2D
+
+
+func is_face() -> bool:
+	return (rank == Ranks.KING
+		 or rank == Ranks.QUEEN
+		 or rank == Ranks.JACK)
